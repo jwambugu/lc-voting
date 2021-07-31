c69e896b2dadb9127c38391e207cdb4cf659a35b
Joram Wambugu <artisan254@gmail.com>
2021-07-31 22:24:50 +0300

    Add test for the show ideas page

---
+++ tests/Feature/ShowIdeasTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Idea;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ShowIdeasTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function list_of_ideas_shows_on_main_page()
+    {
+        $ideaOne = Idea::factory()->create([
+            'title' => 'My First Idea',
+            'description' => 'Description of my first idea',
+        ]);
+
+        $ideaTwo = Idea::factory()->create([
+            'title' => 'My Second Idea',
+            'description' => 'Description of my second idea',
+        ]);
+
+
+        $response = $this->get(route('ideas.index'));
+
+        $response->assertSuccessful();
+        $response->assertSee($ideaOne->title);
+        $response->assertSee($ideaOne->description);
+
+        $response->assertSee($ideaTwo->title);
+        $response->assertSee($ideaTwo->description);
+    }
+}
